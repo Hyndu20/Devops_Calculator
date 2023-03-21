5628d81029267bd76bd1028c513afa66faef8bf4
Hyndavi Ambati <119578178+Hyndu20@users.noreply.github.com>
2023-03-21 22:48:20 +0530

    Delete callingCalculator.java

--- src/main/java/org/example/callingCalculator.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-import org.example.Calculator;
-public class callingCalculator {
-    public static void main(String[] args) {
-        //creating the object and calling the class
-        Calculator calculator = new Calculator(3);
-    }
-}
