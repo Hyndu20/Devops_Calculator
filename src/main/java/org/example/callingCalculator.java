6ad4b2d5e9d18a0b6e7feb80d72e34cc88e5cf34
Hyndavi Ambati <119578178+Hyndu20@users.noreply.github.com>
2023-03-21 10:50:23 +0530

    Create callingCalculator.java

---
+++ src/main/java/org/example/callingCalculator.java
@@ -0,0 +1,8 @@
+package org.example;
+import org.example.Calculator;
+public class callingCalculator {
+    public static void main(String[] args) {
+        //creating the object and calling the class
+        Calculator calculator = new Calculator(3);
+    }
+}
